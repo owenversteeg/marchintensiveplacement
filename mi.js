e8af9ceb06e382fbceea6ae5425d97e789fc96e9
Ezekiel Elin <ezekielelin@me.com>
2014-04-18 14:20:38 -0400

    Now watches for duplicate students

--- mi.js
+++ mi.js
@@ -11,6 +11,154 @@ var studentsFile = 'students.json';
 var outputClassesFile = 'classes-output.json';
 var outputStudentsNotPlaced = 'students-output.json';
 var useGrades = true;
+var studentRandomData = {};
+var studentClassData = {};
+
+function setStudentHasClass(i,s) {
+	studentClassData[i] = s;
+}
+
+function getStudentHasClass(i) {
+	return studentClassData[i];
+}
+
+function readFileSync(file) {
+	return JSON.parse(fs.readFileSync(file, 'utf8'));
+}
+function writeFileSync(file, obj, options) {
+	var str = JSON.stringify(obj, null, module.exports.spaces);
+	return fs.writeFileSync(file, str, options); //not sure if fs.writeFileSync returns anything, but just in case
+}
+
+function getIndexOfStudentName(name) {
+	for (var i = 0; i < students.length; i++) {
+		if (students[i].name == name) {
+			return i;
+		}
+	};
+}
+
+function shuffle(array) {
+	/*shuffles an array*/
+	var currentIndex = array.length
+	, temporaryValue
+	, randomIndex
+	;
+
+	while (0 !== currentIndex) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
+
+function randomIntFromInterval(min,max) {
+	/*Generates a random number from min to max, including*/
+	return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function placeStudent(s,c) {
+	/*places student s in class c, where possible. c is an object, with keys FULL, AM, and PM, s is a string*/	
+	/*terminate if student doesn't exist, throw error if verbose=true*/
+	var si = getIndexOfStudentName(s);
+	if (si == undefined) {
+		if (verbose) {
+			console.log('Index for '+s+' could not be found!');
+		}
+		return false;
+	}
+	if (getStudentHasClass(si)) {
+		return false;
+	}
+	/*terminate if class doesn't exist, throw error if verbose=true*/
+	if (c == undefined) {
+		if (verbose) {
+			console.log('Class for '+s+' could not be found!');
+		}
+		return false;
+	}
+	if (c.FULL !== null && c.AM === null && c.PM === null) {/*if FULLDAY and not AM/PM, assign FULLDAY class if not full etc.*/
+		var cF = c.FULL;
+		try {
+			if (classes[cF].enrolled.length < classes[cF].max) {
+				classes[cF].enrolled.push(s);
+				setStudentHasClass(index,true);
+				if (verbose) {
+					console.log(s+' has been placed in '+cF);
+				}
+			} else {
+				if (verbose) {
+					console.log('Class '+cF+' is full! ('+s+')');
+				}
+				return false;
+			}
+		} catch (err) {
+			console.log('----');
+			console.log('ERROR:'+err.message);
+			console.log(cF);
+			console.log(s);
+		}
+	} else if (c.FULL === null && c.AM !== null && c.PM !== null) {/*IF AM/PM and not FULLDAY assign AM/PM if both are not full*/ 
+		var cAM = c.AM;
+		var cPM = c.PM;
+		try {
+			if ((classes[cAM].enrolled.length < classes[cAM].max) && (classes[cPM].enrolled.length < classes[cPM].max)) {
+				classes[cAM].enrolled.push(s)
+				classes[cPM].enrolled.push(s)
+				setStudentHasClass(index,true);
+				if (verbose) {
+					console.log(s+' has been placed in '+cAM+' & '+cPM);
+				}
+				return true;
+			} else {
+				if (verbose) {
+					console.log('Class '+cPM+' or '+cAM+' is full! ('+s+')');
+				}
+				return false;
+			}
+		} catch (err) {
+			console.log('----');
+			console.log('ERROR:'+err.message);
+			console.log(cAM+', '+cPM);
+			console.log(s);
+		}
+		/*terminate, throw error if verbose=true, also used to mark a user as having a class based on settings*/
+	} else if (c.PM === null && students[si].fordSayre === true) {
+		var cAM = c.AM;
+		try {
+			if ((classes[cAM].enrolled.length < classes[cAM].max)) {
+				classes[cAM].enrolled.push(s)
+				setStudentHasClass(index,true);
+				if (verbose) {
+					console.log(s+' has been placed in '+cAM+' (Ford Sayre)');
+				}
+			} else {
+				if (verbose) {
+					console.log('Class '+cAM+' is full! ('+s+' Ford Sayre)');
+				}
+				return false;
+			}
+		} catch (err) {
+			console.log('----');
+			console.log('ERROR:'+err.message);
+			console.log(cAM);
+			console.log(s);
+		}
+	} else {
+		if (verbose) {
+			console.log('The class data for '+s+' is not valid.');
+		}
+		if (doNotAssignClassesWhenNoneAreRequested) {
+			setStudentHasClass(index,true)
+		}
+	}
+	return true;
+}
 
 if (process.argv.indexOf('--verbose') !== -1) {
 	verbose = true;
@@ -50,143 +198,24 @@ if (process.argv.indexOf('--help') !== -1) {
 var classes = readFileSync(classFile);
 var students = readFileSync(studentsFile);
 
-function readFileSync(file) {
-	return JSON.parse(fs.readFileSync(file, 'utf8'));
-}
-function writeFileSync(file, obj, options) {
-	var str = JSON.stringify(obj, null, module.exports.spaces);
-	return fs.writeFileSync(file, str, options); //not sure if fs.writeFileSync returns anything, but just in case
-}
-
-function shuffle(array) {
-	/*shuffles an array*/
-	var currentIndex = array.length
-	, temporaryValue
-	, randomIndex
-	;
-
-	while (0 !== currentIndex) {
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
-
-		temporaryValue = array[currentIndex];
-		array[currentIndex] = array[randomIndex];
-		array[randomIndex] = temporaryValue;
-	}
-
-	return array;
-}
-
-function randomIntFromInterval(min,max) {
-	/*Generates a random number from min to max, including*/
-	return Math.floor(Math.random()*(max-min+1)+min);
-}
-
-function placeStudent(s,c) {
-	/*places student s in class c, where possible. c is an object, with keys FULL, AM, and PM, s is a string*/
-	
-	/*find the student's index in students[]*/
-	var si;
-	for (var i = 0; i < students.length; i++) {
-		if (students[i].name = s) {
-			si = i;
-			break;
-		}
-	};
-	/*terminate if student doesn't exist, throw error if verbose=true*/
-	if (si == undefined) {
-		if (verbose) {
-			console.log('Index for '+s+' could not be found!');
-		}
-		return false;
-	}
-	/*terminate if class doesn't exist, throw error if verbose=true*/
-	if (c == undefined) {
-		if (verbose) {
-			console.log('Class for '+s+' could not be found!');
-		}
-		return false;
-	}
-	if (c.FULL !== null && c.AM === null && c.PM === null) {/*if FULLDAY and not AM/PM, assign FULLDAY class if not full etc.*/
-		var cF = c.FULL;
-		try {
-			if (classes[cF].enrolled.length < classes[cF].max) {
-				classes[cF].enrolled.push(s);
-				students[index].hasClass = true;
-				if (verbose) {
-					console.log(s+' has been placed in '+cF);
-				}
-			} else {
-				if (verbose) {
-					console.log('Class '+cF+' is full! ('+s+')');
-				}
-				return false;
-			}
-		} catch (err) {
-			console.log('----');
-			console.log('ERROR:'+err.message);
-			console.log(cF);
-			console.log(s);
-		}
-	} else if (c.FULL === null && c.AM !== null && c.PM !== null) {/*IF AM/PM and not FULLDAY assign AM/PM if both are not full*/ 
-		var cAM = c.AM;
-		var cPM = c.PM;
-		try {
-			if ((classes[cAM].enrolled.length < classes[cAM].max) && (classes[cPM].enrolled.length < classes[cPM].max)) {
-				classes[cAM].enrolled.push(s)
-				classes[cPM].enrolled.push(s)
-				students[index].hasClass = true;
-				if (verbose) {
-					console.log(s+' has been placed in '+cAM+' & '+cPM);
-				}
-			} else {
-				if (verbose) {
-					console.log('Class '+cPM+' or '+cAM+' is full! ('+s+')');
-				}
-				return false;
-			}
-		} catch (err) {
-			console.log('----');
-			console.log('ERROR:'+err.message);
-			console.log(cAM+', '+cPM);
-			console.log(s);
-		}
-		/*terminate, throw error if verbose=true, also used to mark a user as having a class based on settings*/
-	} else if (c.PM === null && students[si].fordSayre === true) {
-		var cAM = c.AM;
-		try {
-			if ((classes[cAM].enrolled.length < classes[cAM].max)) {
-				classes[cAM].enrolled.push(s)
-				students[index].hasClass = true;
-				if (verbose) {
-					console.log(s+' has been placed in '+cAM+' (Ford Sayre)');
-				}
-			} else {
-				if (verbose) {
-					console.log('Class '+cAM+' is full! ('+s+' Ford Sayre)');
-				}
-				return false;
-			}
-		} catch (err) {
-			console.log('----');
-			console.log('ERROR:'+err.message);
-			console.log(cAM);
-			console.log(s);
+var studentRecordsForDuplicates = [];
+for (var i = 0; i < students.length; i++) {
+	if (studentRecordsForDuplicates.indexOf(students[i].name) !== -1) {
+		if (process.argv.indexOf('--force') !== -1) {
+			students.splice(i,1);
+		} else {
+			console.log(students[i].name+' was found twice, names must be unique');
+			console.log('Use --force to remove ignore duplicates');
+			process.kill();
 		}
 	} else {
-		if (verbose) {
-			console.log('The class data for '+s+' is not valid.');
-		}
-		if (doNotAssignClassesWhenNoneAreRequested) { /*If a user doesn't request any classes, they will not be given one and no error will be output*/
-			students[index].hasClass = true;
+		studentRecordsForDuplicates.push(students[i].name);
 	}
-}
-return true;
-}
+};
 
 /*prepares data, hasClass, shouldn't be passed, will be overritten*/
 for (var i = 0; i < students.length; i++) {
-	students[i].hasClass = false;
+	setStudentHasClass(i,false);
 };
 /*randomize student indexes*/
 while (studentIndex.length < students.length) {
@@ -197,53 +226,57 @@ while (studentIndex.length < students.length) {
 }
 /*four loops, one for each grade*/
 for (var x = 0; x < grades.length; x++) {
-	/*two loops, first time won't assign classes not requests*/
-	for (var y = 0; y < 2; y++) {
+	/*three loops, first time won't assign classes not requests*/
+	for (var y = 0; y < 3; y++) {
 		/*go through random student array*/
 		for (var i = 0; i < studentIndex.length; i++) {
 			var index = studentIndex[i];
 			if (students[index].grade == grades[x] || !useGrades) {
 				/*if the student has no class, try to assing their requests*/
-				if (students[index].hasClass !== true) {
+				if (getStudentHasClass(index) !== true) {
 					/*going from request 1, to last request*/
 					for (var n = 0; n < students[index].choices.length; n++) {
-						if (placeStudent(students[index].name,students[index].choices[n],index)) {
+						if (placeStudent(students[index].name,students[index].choices[n])) {
 							happyness.push(n);
 							break;
 						}
 					}
 				}
-				/*if the student has no class, and not the first loop, assign to random class*/
-				if (students[index].hasClass !== true && y !== 0) {
-					/*loop classes*/
-					for (var n = 0; n < Object.keys(classes).length; n++) {
-						var key = Object.keys(classes)[n];
-						/*find first FULLDAY type class that isn't empty.*/
-						if (classes[key].type == 'FULL') {
-							if (placeStudent(students[index].name,{"FULL":key,"AM":null,"PM":null},index)) {
-								placed = true;
-								happyness.push(students[index].choices.length);
-								break;
+				if ((randomIntFromInterval(1,2) === 1 || studentRandomData[index] === 2) && studentRandomData[index] !== 1) {
+					studentRandomData[index] = 1;
+					/*if the student has no class, and not the first loop, assign to random class*/
+					if (getStudentHasClass(index) !== true && y !== 0) {
+						/*loop classes*/
+						for (var n = 0; n < Object.keys(classes).length; n++) {
+							var key = Object.keys(classes)[n];
+							/*find first FULLDAY type class that isn't empty.*/
+							if (classes[key].type == 'FULL') {
+								if (placeStudent(students[index].name,{"FULL":key,"AM":null,"PM":null})) {
+									happyness.push(students[index].choices.length);
+									break;
+								}
 							}
 						}
-					}
-				}
-				if (students[index].hasClass !== true && y !== 0) {
-					/*loop classes*/
-					for (var n = 0; n < Object.keys(classes).length; n++) {
-						var keyAM = Object.keys(classes)[n];
-						/*find first AM type class that isn't empty.*/
-						if (classes[keyAM].type == 'AM') {
-							for (var z = 0; z < Object.keys(classes).length; z++) {
-								var keyPM = Object.keys(classes)[z];
-								/*find first PM type class that isn't empty.*/
-								if (classes[keyPM].type == 'PM') {
-									if (placeStudent(students[index].name,{"FULL":null,"AM":keyAM,"PM":keyPM},index)) {
-										happyness.push(students[index].choices.length);
-										if (verbose) {
-											console.log('Placed AM/PM Random Class!');
+					}					
+				} else {
+					studentRandomData[index] = 2;
+					if (getStudentHasClass(index) !== true && y !== 0) {
+						/*loop classes*/
+						for (var n = 0; n < Object.keys(classes).length; n++) {
+							var keyAM = Object.keys(classes)[n];
+							/*find first AM type class that isn't empty.*/
+							if (classes[keyAM].type == 'AM') {
+								for (var z = 0; z < Object.keys(classes).length; z++) {
+									var keyPM = Object.keys(classes)[z];
+									/*find first PM type class that isn't empty.*/
+									if (classes[keyPM].type == 'PM') {
+										if (placeStudent(students[index].name,{"FULL":null,"AM":keyAM,"PM":keyPM})) {
+											happyness.push(students[index].choices.length);
+											if (verbose) {
+												console.log('Placed AM/PM Random Class!');
+											}
+											break;
 										}
-										break;
 									}
 								}
 							}
@@ -251,7 +284,7 @@ for (var x = 0; x < grades.length; x++) {
 					}
 				}
 				/*If unable to place the student, put them on the unplaceable list*/
-				if (students[index].hasClass !== true && y !== 0) {
+				if (getStudentHasClass(index) !== true && y !== 0) {
 					studentUnplaceableIndex.push(index);
 				}
 			} else {
@@ -276,4 +309,4 @@ for (var i = 0; i < happyness.length; i++) {
 console.log(total/happyness.length);
 /*write files*/
 writeFileSync(outputClassesFile, classes);
-writeFileSync(outputStudentsNotPlaced, studentUnplaceableIndex);
+writeFileSync(outputStudentsNotPlaced, students);
