f648bc3b44ff31c47069837cb5dc4b9d6a330a89
Ezekiel Elin <ezekielelin@me.com>
2014-04-23 19:21:38 -0400

    Update to MI, moving to conversion.js

--- conversion.js
+++ conversion.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 var studentIndex = []; /*Used to randomly order students*/
 var studentUnplaceableIndex = []; /*Used to track students that were not assigned to a class*/
-var happyness = []; /*Used to track how happy students are with their seleciton*/
+var happiness = []; /*Used to track how happy students are with their seleciton*/
 var grades = [ 12, 11, 10, 9 ];
 var doNotAssignClassesWhenNoneAreRequested = true; /*If a user doesn't request any classes, they will not be given one and no error will be output*/
 var verbose = false; /*Output operations, triggerable with --verbose as well*/
@@ -19,20 +19,76 @@ function setStudentHasClass(i,s) {
 }
 
 function getStudentHasClass(i) {
-	return studentClassData[i];
+	return verifyStudentHasClass(i);
+	//return studentClassData[i];
 }
 
-function readFileSync(file) {
-	return JSON.parse(fs.readFileSync(file, 'utf8'));
+function verifyStudentHasClass(i) {
+	var name = students[i].name;
+
+	var hasAM = false;
+	var hasFULL = false;
+	var hasPM = false;
+	for (var i = 0; i < classes.length; i++) {
+		if (classes[i].choices.indexOf(name) !== undefined) {
+			if (classes[i].type === 'FULL') {
+				hasFULL = true;
+			}
+			if (classes[i].type === 'AM') {
+				hasAM = true;
+			}
+			if (classes[i].type === 'PM') {
+				hasPM = true;
+			}
+		}
+		if (hasAM && hasPM) {
+			return true;
+		}
+		if (hasFULL) {
+			return true;
+		}
+	};
+	return false;
 }
-function writeFileSync(file, obj, options) {
-	var str = JSON.stringify(obj, null, module.exports.spaces);
-	return fs.writeFileSync(file, str, options); //not sure if fs.writeFileSync returns anything, but just in case
+
+function readJSONSync(file) {
+	try {
+		return JSON.parse(fs.readFileSync(file, 'utf8'));
+	} catch (err) {
+		console.log('ERROR:'+err.message);
+		return false;
+	}
+}
+
+function readCSVSync(file) {
+	try {
+		return fs.readFileSync(file, 'utf8');
+	} catch (err) {
+		console.log('ERROR:'+err.message);
+		return false;
+	}
+}
+function writeJSONSync(file, obj, options) {
+	try {
+		var str = JSON.stringify(obj, null, module.exports.spaces);
+		return fs.writeFileSync(file, str, options);
+	} catch (err) {
+		console.log('ERROR:'+err.message);
+		return false;
+	}
+}
+function writeCSVSync(file, str, options) {
+	try {
+		return fs.writeFileSync(file, str, options);
+	} catch (err) {
+		console.log('ERROR:'+err.message);
+		return false;
+	}
 }
 
 function getIndexOfStudentName(name) {
 	for (var i = 0; i < students.length; i++) {
-		if (students[i].name == name) {
+		if (students[i].name === name) {
 			return i;
 		}
 	};
@@ -66,7 +122,7 @@ function placeStudent(s,c) {
 	/*places student s in class c, where possible. c is an object, with keys FULL, AM, and PM, s is a string*/	
 	/*terminate if student doesn't exist, throw error if verbose=true*/
 	var si = getIndexOfStudentName(s);
-	if (si == undefined) {
+	if (si === undefined) {
 		if (verbose) {
 			console.log('Index for '+s+' could not be found!');
 		}
@@ -76,7 +132,7 @@ function placeStudent(s,c) {
 		return false;
 	}
 	/*terminate if class doesn't exist, throw error if verbose=true*/
-	if (c == undefined) {
+	if (c === undefined) {
 		if (verbose) {
 			console.log('Class for '+s+' could not be found!');
 		}
@@ -160,6 +216,109 @@ function placeStudent(s,c) {
 	return true;
 }
 
+function splitCSVRow(row) {
+	var deleteIndexes = [];
+
+	row = row.replace(/,,/g,',"",').replace(/,,/g,',"",').replace(/,,/g,',"",');
+
+	if (row.substr(row.length - 1) == ',') {
+		row = row + '""';
+	}
+
+	if (row.charAt(0) == ',') {
+		row = '""' + row;
+	}
+
+
+	row = row.match(/"([^"]|"")*"|[^,\n]*/g);/*"*/
+
+	for (var i = 0; i < row.length; i++) {
+		if (row[i].charAt(0) == '"') {
+			row[i] = row[i].substr(1);
+		}
+		if (row[i].charAt(row[i].length-1) == '"') {
+			row[i] = row[i].substr(0,row[i].length-1);
+		}
+		if (i%2) {
+			deleteIndexes.push(i);
+		}
+	}
+
+	for (var i = deleteIndexes.length - 1; i >= 0 ; i--) {
+		row.splice(deleteIndexes[i],1);
+	}
+	return row;
+}
+
+function convertRowToJSON(row,head) {
+	obj = {"name":"","fordSayre":false,"hartfordTechAM":false,"hartfordTechPM":false,"grade":9,"choices":[{},{},{},{},{},{},{},{}]};
+	var row = splitCSVRow(row);
+	var head = head.split(',');
+	for (var i = 0; i < row.length; i++) {
+		if (head[i] === 'name') {
+			obj.name = row[i];
+		}
+		if (head[i] === 'hartfordTech') {
+			if (row[i] === 'AM') {
+				obj.hartfordTechAM = true;
+				obj.hartfordTechPM = false;
+			} else if (row[i] === 'PM') {
+				obj.hartfordTechPM = true;
+				obj.hartfordTechAM = false;
+			} else {
+				obj.hartfordTechPM = false;
+				obj.hartfordTechAM = false;
+			}
+		}
+		if (head[i] === 'fordSayre') {
+			if (row[i] === "false") {
+				obj.fordSayre === false;
+			}
+			if (row[i] === "true") {
+				obj.fordSayre === true;
+			}
+		}
+		if (head[i] === 'commonGround') {
+			obj.commonGround = row[i];
+		}
+		if (head[i] === 'grade') {
+			if (row[i].indexOf(9) !== -1) {
+				obj.grade = 9;
+			} else if (row[i].indexOf(10) !== -1) {
+				obj.grade = 10;
+			} else if (row[i].indexOf(11) !== -1) {
+				obj.grade = 11;
+			} else if (row[i].indexOf(12) !== -1) {
+				obj.grade = 12;
+			} else {
+				obj.grade = 9;
+			}
+		}
+		for (var x = 1; x < 10; x++) {
+			if (head[i] === x+'FULL') {
+				if (row[i] === "") {
+					obj.choices[x-1].FULL = null;
+				} else {
+					obj.choices[x-1].FULL = row[i];					
+				}
+			} else if (head[i] === x+'AM') {
+				if (row[i] === "") {
+					obj.choices[x-1].AM = null;
+				} else {
+					obj.choices[x-1].AM = row[i];					
+				}
+			} else if (head[i] === x+'PM') {
+				if (row[i] === "") {
+					obj.choices[x-1].PM = null;
+				} else {
+					obj.choices[x-1].AM = row[i];
+				}
+			}
+		}
+	}
+	return obj;
+}
+
 if (process.argv.indexOf('--verbose') !== -1) {
 	verbose = true;
 }
@@ -183,20 +342,58 @@ if (process.argv.indexOf('--help') !== -1) {
 	console.log('Show all actions');
 	console.log('	--verbose');
 	console.log('Classes Input File');
-	console.log('	--classes [file.json]');
+	console.log('	--classes [classes.json]');
 	console.log('Students Input File');
-	console.log('	--students [file.json]');
+	console.log('	--students [students.json]');
 	console.log('Classes Output File');
-	console.log('	--output-classes [file.json]');
+	console.log('	--output-classes [classes-output.json]');
 	console.log('Students Not Placed Output File');
-	console.log('	--output-studentsNotPlaced [file.json]');
+	console.log('	--output-studentsNotPlaced [students-output.json]');
+	console.log('Convert CSV to JSON');
+	console.log('	--convert-csv-json [OPTIONAL students.csv] [OPTIONAL students.json]');
 	console.log('Ignore .grade field');
 	console.log('	--ignore-grades');
 	process.kill()
 }
 
-var classes = readFileSync(classFile);
-var students = readFileSync(studentsFile);
+if (process.argv.indexOf('--convert-csv-json') !== -1) {
+	var csv = "";
+	if (process.argv[process.argv.indexOf('--convert-csv-json')+1] !== undefined) {
+		csv = readCSVSync(process.argv[process.argv.indexOf('--convert-csv-json')+1]);
+	} else {
+		csv = readCSVSync('students.csv');
+	}
+	var csvOutputJsonFileName = 'students.json';
+	if (process.argv[process.argv.indexOf('--convert-csv-json')+2] !== undefined) {
+		csvOutputJsonFileName = process.argv[process.argv.indexOf('--convert-csv-json')+2];
+	}
+	var csvAsJSON = [];
+
+	if (csv.length === 0 || !csv) {
+		console.log('The provided CSV file is empty, corrupt, or doesn\'t exist');
+		process.kill();
+	}
+
+	//csv = csv.replace(/,,/g,',"",').replace(/,,/g,',"",').replace(/,,/g,',"",');/*To prevent overlaps from causing issues*/
+	csv = csv.split('\n');
+	
+	for (var i = 0; i < csv.length; i++) {
+		if (i === 0) {
+			console.log('Skipping row 1, assuming header row');
+		} else {
+			var row = convertRowToJSON(csv[i],csv[0]);
+			if (row !== false) {
+				csvAsJSON.push(row);
+			}
+		}
+	}
+	writeJSONSync(csvOutputJsonFileName,csvAsJSON);
+	console.log('Wrote file '+csvOutputJsonFileName+' based on data from provided CSV file');
+	process.kill();
+}
+
+var classes = readJSONSync(classFile);
+var students = readJSONSync(studentsFile);
 
 var studentRecordsForDuplicates = [];
 for (var i = 0; i < students.length; i++) {
@@ -231,13 +428,13 @@ for (var x = 0; x < grades.length; x++) {
 		/*go through random student array*/
 		for (var i = 0; i < studentIndex.length; i++) {
 			var index = studentIndex[i];
-			if (students[index].grade == grades[x] || !useGrades) {
+			if (students[index].grade === grades[x] || !useGrades) {
 				/*if the student has no class, try to assing their requests*/
 				if (getStudentHasClass(index) !== true) {
 					/*going from request 1, to last request*/
 					for (var n = 0; n < students[index].choices.length; n++) {
 						if (placeStudent(students[index].name,students[index].choices[n])) {
-							happyness.push(n);
+							happiness.push(n);
 							break;
 						}
 					}
@@ -250,9 +447,9 @@ for (var x = 0; x < grades.length; x++) {
 						for (var n = 0; n < Object.keys(classes).length; n++) {
 							var key = Object.keys(classes)[n];
 							/*find first FULLDAY type class that isn't empty.*/
-							if (classes[key].type == 'FULL') {
+							if (classes[key].type === 'FULL') {
 								if (placeStudent(students[index].name,{"FULL":key,"AM":null,"PM":null})) {
-									happyness.push(8);
+									happiness.push(8);
 									break;
 								}
 							}
@@ -265,13 +462,13 @@ for (var x = 0; x < grades.length; x++) {
 						for (var n = 0; n < Object.keys(classes).length; n++) {
 							var keyAM = Object.keys(classes)[n];
 							/*find first AM type class that isn't empty.*/
-							if (classes[keyAM].type == 'AM') {
+							if (classes[keyAM].type === 'AM') {
 								for (var z = 0; z < Object.keys(classes).length; z++) {
 									var keyPM = Object.keys(classes)[z];
 									/*find first PM type class that isn't empty.*/
-									if (classes[keyPM].type == 'PM') {
+									if (classes[keyPM].type === 'PM') {
 										if (placeStudent(students[index].name,{"FULL":null,"AM":keyAM,"PM":keyPM})) {
-											happyness.push(8);
+											happiness.push(8);
 											if (verbose) {
 												console.log('Placed AM/PM Random Class!');
 											}
@@ -303,10 +500,10 @@ for (var i = 0; i < studentUnplaceableIndex.length; i++) {
 var percentage = (studentIndex.length-studentUnplaceableIndex.length)/studentIndex.length*100;
 console.log(Math.round(percentage)+'% placement, where '+(studentIndex.length - studentUnplaceableIndex.length)+' student(s) of '+studentIndex.length+' were placed');
 var total = 0;
-for (var i = 0; i < happyness.length; i++) {
-	total += happyness[i]
+for (var i = 0; i < happiness.length; i++) {
+	total += happiness[i]
 };
-console.log(Math.round(100-((total/happyness.length)/8*100))+'%');
+console.log(Math.round(100-((total/happiness.length)/8*100))+'%');
 /*write files*/
-writeFileSync(outputClassesFile, classes);
-writeFileSync(outputStudentsNotPlaced, studentUnplaceableIndex);
+writeJSONSync(outputClassesFile, classes);
+writeJSONSync(outputStudentsNotPlaced, studentUnplaceableIndex);
