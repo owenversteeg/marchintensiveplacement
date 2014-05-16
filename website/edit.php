c1f1e7507a9acbbcb169edf586e70bcb034aaf2c
Ezekiel Elin <ezekielelin@me.com>
2014-05-16 16:48:07 -0400

    Pull from MI server

---
+++ website/edit.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>March Intensive</title>
+	<link rel="stylesheet" href="/marchintensive/css/font-awesome.min.css">
+	<link rel="stylesheet" href="/marchintensive/css/bootstrap.min.css">
+	<script type="text/javascript" src="/marchintensive/js/jquery.min.js"></script>
+	<script type="text/javascript" src="/marchintensive/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div id="container" style="margin-top:50px" class="container">
+		<?php
+		$classes = json_decode(file_get_contents('data/classes.json'),true);
+		$students = json_decode(file_get_contents('data/students.json'),true);
+
+		foreach ($classes as $className => $class) {
+			?>
+			<div class="row well">
+				<div class="col-md-6">
+				<?php
+						echo '<h4>'.$className.'</h4>';
+				?>
+				</div>
+				<div class="col-md-6">
+					<?php
+					foreach ($class['enrolled'] as $key => $student) {
+						echo '<h4>'.$student.'</h4><br>';			
+					}
+					?>
+				</div>
+			</div>
+			<?php
+		}
+		?>
+	</div>
+</body>
+</html>
---
+++ website/editAction.php
@@ -0,0 +1,27 @@
+<?php
+$classes = json_decode(file_get_contents('data/classes.json'),true);
+if (@$_GET['operation'] == 'add') {
+	if (@$_GET['user'] != "") {
+		if (@$_GET['class'] != "") {
+			if (!in_array(@$_GET['user'], $classes[@$_GET['class']])))
+				push_array($classes[@$_GET['class']],@$_GET['user']);
+			} else {
+				$error = "Student already in class";
+			}
+		} else {
+			$error = "No class sent (GET CLASS)";
+		}
+	} else {
+		$error = "No user sent (GET USER)";
+	}
+} else {
+	$error = "No operation set (GET OPERATION)";
+}
+if (@$error != "") {
+	echo $error;
+	echo '<br><br><a href="/marchintensive/index.php">Click here to go back</a>';
+} else {
+	file_put_contents('data/classes.json', json_encode($classes));
+	header('Location:/marchintensive/index.php');
+}
+?>
