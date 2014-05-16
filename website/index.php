a359545695e69cee28e544e99e1deec2a2327222
Ezekiel Elin <ezekielelin@me.com>
2014-05-16 19:13:51 -0400

    Labels

--- website/index.php
+++ website/index.php
@@ -290,7 +290,7 @@ if (array_key_exists("submitting", $_POST)) {
 				for ($i = 0; $i < $choices; $i ++) {
 					?>
 					<div id="choice<?php echo $i; ?>" class="row row-margin-top row-margin-bottom">
-						<div class="col-md-2">Choice <?php echo($i + 1);?>:</div>
+						<div class="col-md-2"><label class="control-label">Choice <?php echo($i + 1);?>:</label></div>
 						<div class="col-md-4"><select class="form-control" id="select-full-<?php echo($i);?>" select-num="<?php echo($i);?>" name="full[<?php echo($i);?>]"></select></div>
 						<div class="col-md-3"><select class="form-control" id="select-am-<?php echo($i);?>" select-num="<?php echo($i);?>" name="am[<?php echo($i);?>]"></select></div>
 						<div class="col-md-3"><select class="form-control" id="select-pm-<?php echo($i);?>" select-num="<?php echo($i);?>" name="pm[<?php echo($i);?>]"></select></div>
