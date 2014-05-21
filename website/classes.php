2bc0aa3179aa18ba9eabfd15a838a30a7f70fdf1
Owen Versteeg <owen.versteeg@live.com>
2014-05-21 12:24:09 -0400

    replace crap with header

--- website/classes.php
+++ website/classes.php
@@ -6,14 +6,7 @@ if (!file_exists('data/classes.json')) {
 	$classes = json_decode(file_get_contents('data/classes.json'),true);
 }
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>March Intensive</title>
-	<link rel="stylesheet" href="css/custom.css">
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-	<script type="text/javascript" src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+include 'header.php';
 	<style>
 		.divider {
 			background:#89d084;
