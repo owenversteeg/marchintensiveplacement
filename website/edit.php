001b95a661dbebb4ea0b2171bde274126a9f9ae1
Owen Versteeg <owen.versteeg@live.com>
2014-05-21 12:23:44 -0400

    include header

--- website/edit.php
+++ website/edit.php
@@ -4,14 +4,7 @@ if (!file_exists('data/classes.json') || !file_exists('data/students.json') || !
 	exit;
 }
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>March Intensive</title>
-	<link rel="stylesheet" href="css/custom.css">
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-	<script type="text/javascript" src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+include 'header.php';
 	<script>
 		function sendNetworkRequest(operation,user,cl,obj) {
 			$.ajax({
