07bbad278b55cb0684bebb63ba3f4442963b8b70
Ezekiel Elin <ezekielelin@me.com>
2014-05-21 17:17:03 -0400

    Per @CouleeApps 's request

--- website/header.php
+++ website/header.php
@@ -6,3 +6,4 @@
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 	<script type="text/javascript" src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
