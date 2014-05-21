48e971f55264a53dd522300d194001cec377461a
Owen Versteeg <owen.versteeg@live.com>
2014-05-21 12:25:10 -0400

    replace with header

--- website/index.php
+++ website/index.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>March Intensive</title>
-	<link rel="stylesheet" href="css/custom.css">
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-	<script type="text/javascript" src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+include 'header.php';
 </head>
 <body>
 	<div id="container" class="container">
