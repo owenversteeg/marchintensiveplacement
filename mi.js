4bc7b6aecdeff039d582c6eecf50b5536fa4ce0e
Owen Versteeg <owen.versteeg@live.com>
2014-05-22 07:07:48 -0400

    fix ezekiel's comments

--- mi.js
+++ mi.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
-var doNotAssignClassesWhenNoneAreRequested = true; /*If a user doesn't request any classes, they will not be given one and no error will be output*/
-var verbose = false; /*Output operations, triggerable with --verbose as well*/
+var doNotAssignClassesWhenNoneAreRequested = true; // If a user doesn't request any classes, they will not be given one and no error will be output
+var verbose = false; // Output operations, triggerable with --verbose as well
 var classFile = 'classes.json', studentsFile = 'students.json', useGrades = true, studentRandomData = {}, studentClassData = {}, grades = [], happiness = [], studentUnplaceableIndex = [];
 
 function setStudentHasClass(id, s) { studentClassData[id] = s; return true; }
@@ -45,7 +45,7 @@ function getIndexOfStudentID(id) {
 }
 
 function shuffle(array) {
-	/*shuffles an array*/
+	// shuffles an array
 	var currentIndex = array.length, temporaryValue, randomIndex;
 
 	while (0 != currentIndex) {
@@ -78,8 +78,8 @@ function classRequestDetails(det, cl) {
 }
 
 function placeStudent(sid, c) {
-	/*places student s in class c, where possible. c is an object, with keys full, am, and pm, s is a string*/	
-	/*terminate if student doesn't exist, throw error if verbose=true*/
+	// places student s in class c, where possible. c is an object, with keys full, am, and pm, s is a string
+	// terminate if student doesn't exist, throw error if verbose=true
 	var studentIndex = getIndexOfStudentID(sid), requestClasses = [];
 	if (!studentIndex) {
 		vlog('Index for '+getNameOfStudentID(sid)+' could not be found!');
@@ -88,12 +88,12 @@ function placeStudent(sid, c) {
 	if (getStudentHasClass(sid)) {
 		return false;
 	}
-	/*terminate if class doesn't exist, throw error if verbose=true*/
+	// terminate if class doesn't exist, throw error if verbose=true
 	if (!c) {
 		vlog('Class for '+getNameOfStudentID(sid)+' could not be found!');
 		return false;
 	}
-	/*verify all possible combinations of class choices and fordSayre/hartfordTech settings*/
+	// verify all possible combinations of class choices and fordSayre/hartfordTech settings
 	if (students[studentIndex].hartfordTech === 'none' && !students[studentIndex].fordSayre) {
 		requestClasses = requestClasses.concat(c.full || [c.am, c.pm]);
 	} else if (c.am && students[studentIndex].hartfordTech == 'none' && students[studentIndex].fordSayre == true) {
@@ -185,7 +185,7 @@ for (var i = 0; i < students.length; i++) {
 }
 
 for (var i=0; i < students.length; i++) {
-	/*Set ford sayre values*/
+	// Set ford sayre values
 	students[i].fordSayre = !!students[i].fordSayre;
 	if (students[i].hartfordTech != 'none' && students[i].hartfordTech != 'am' && students[i].hartfordTech != 'pm') {
 		students[i].hartfordTech = 'none'
@@ -211,25 +211,25 @@ for (var i=0; i < students.length; i++) {
 		}
 	}
 }
-//prepares data, hasClass
+// prepares data, hasClass
 for (var i = 0; i < students.length; i++) {
 	setStudentHasClass(students[i].studentid, false);
 }
-//randomize student indexes
+// randomize student indexes
 students = shuffle(students);
 
-//four loops, one for each grade
+// four loops, one for each grade
 for (var x = 0; x < grades.length; x++) {
-	/*go through random student array*/
+	// go through random student array
 	for (var i = 0; i < students.length; i++) {
 		var sid = students[i].studentid;
 		if ((students[i].grade == grades[x] || !useGrades) && !getStudentHasClass(sid)) {
-			/*if the student has no class, try to assing their requests*/
-			/*going from request 1, to last request*/
+			// if the student has no class, try to assing their requests
+			// going from request 1, to last request
 			for (var n = 0; n < students[i].choices.length; n++) {
 				if (placeStudent(students[i].studentid,students[i].choices[n])) {
 					if (n != 0) {
-						//place students on waitlist
+						// place students on waitlist
 						if (classRequestDetails('type',students[i].choices[n-1]) == 'ampm') {
 							if (students[i].choices[n-1].am != undefined) {
 								if (classes[students[i].choices[n-1].am].waitlist == undefined) {
@@ -260,27 +260,27 @@ for (var x = 0; x < grades.length; x++) {
 	}
 }
 for (var i = 0; i < students.length; i++) {
-	/*If unable to place the student, put them on the unplaceable list*/
+	// If unable to place the student, put them on the unplaceable list
 	if (!getStudentHasClass(students[i].studentid)) {
 		studentUnplaceableIndex.push(students[i].studentid);
 		happiness.push(8);
 	}
 };
-/*look through the student unplaceable index, and replace it with names so that it can be read easier*/
+// look through the student unplaceable index, and replace it with names so that it can be read easier
 for (var i = 0; i < studentUnplaceableIndex.length; i++) {
 	studentUnplaceableIndex[i] = getNameOfStudentID(studentUnplaceableIndex[i]);
 }
-/*remove dupe waitlists*/
+// remove dupe waitlists
 for (var i = 0; i < Object.keys(classes).length; i++) {
 	var key = Object.keys(classes);
 	if (classes[key[i]].waitlist != undefined) {
 		classes[key[i]].waitlist = removeDuplicatesFromArray(classes[key[i]].waitlist);
 	}
 }
-//print the success percentage to two decimal places
+// print the success percentage to two decimal places
 var percentage = Math.round((students.length-studentUnplaceableIndex.length)/students.length*10000)/100;
 if (studentUnplaceableIndex != 0 && percentage == 100) {
-	percentage = 99; //you liar
+	percentage = 99; // you liar
 }
 console.log(percentage+'% placement, where '+(students.length - studentUnplaceableIndex.length)+' student(s) of '+students.length+' were placed');
 var total = 0;
@@ -301,10 +301,10 @@ for (var i = 0; i < Object.keys(classes).length; i++) {
 		classes[classKey].enrolled[n] = getNameOfStudentID(classes[classKey].enrolled[n]);
 	}
 }
-//console.log(Math.round(100-((total/happiness.length)/8*100))+'% Happiness');
-//console.log(totalZero);
+// console.log(Math.round(100-((total/happiness.length)/8*100))+'% Happiness');
+// console.log(totalZero);
 
-//write files
+// write files
 writeJSON(outputClassesFile, classes);
 writeJSON(outputStudents, students);
 writeJSON(outputStudentsNotPlaced, studentUnplaceableIndex);
